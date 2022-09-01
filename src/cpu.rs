ba21d91e3f42d0274013a5dc227f86cc1caf3fb2
LePretzel <vince0782@gmail.com>
2022-09-01 14:07:15 -0500

    Implement ld r r' instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use crate::memory::Memory;
 
 pub struct CPU {
@@ -17,11 +19,13 @@ pub struct CPU {
     stack_pointer: u16,
     program_counter: u16,
     memory: [u8; 0xFFFF],
+    instructions: [Option<Rc<dyn Fn(&mut Self) -> ()>>; 0xFF],
 }
 
 impl CPU {
     pub fn new() -> Self {
-        CPU {
+        const init_instruction: Option<Rc<dyn Fn(&mut CPU) -> ()>> = None;
+        let mut cpu = CPU {
             register_a: 0x11,
             register_f: 0x80,
             register_b: 0x00,
@@ -33,6 +37,62 @@ impl CPU {
             stack_pointer: 0x0100,
             program_counter: 0xFFFE,
             memory: [0; 0xFFFF],
+            instructions: [init_instruction; 0xFF],
+        };
+
+        // LD instructions
+        // LD r, r' (each 1 M-cycle)
+        for i in 0..8 {
+            for j in 0..8 {
+                let source_num = j as u8;
+                let dest_num = i as u8;
+                let opcode: u8 = 0b01000000 | source_num | (dest_num << 3);
+
+                cpu.instructions[opcode as usize] = Some(Rc::new(move |cpu: &mut CPU| {
+                    let source_option = cpu.get_register(source_num);
+                    if source_option.is_some() {
+                        let source = *source_option.unwrap();
+                        let dest_option = cpu.get_register(dest_num);
+                        if dest_option.is_some() {
+                            let dest = dest_option.unwrap();
+                            *dest = source;
+                        }
+                    }
+                }));
+            }
+        }
+
+        // LD A (DE)
+        cpu.instructions[0x1A] = Some(Rc::new(|cpu: &mut CPU| {
+            // 2 M-cycles
+            let source = cpu.read(CPU::combine_bytes(cpu.register_d, cpu.register_e));
+            cpu.register_a = source;
+        }));
+        cpu
+    }
+
+    pub fn execute(&mut self, opcode: u8) {
+        if self.instructions[opcode as usize].is_none() {
+            return;
+        }
+        let inst = self.instructions[opcode as usize].as_ref().unwrap().clone();
+        inst(self);
+    }
+
+    fn combine_bytes(high: u8, low: u8) -> u16 {
+        ((high as u16) << 8) | low as u16
+    }
+
+    fn get_register(&mut self, code: u8) -> Option<&mut u8> {
+        match code {
+            0 => Some(&mut self.register_b),
+            1 => Some(&mut self.register_c),
+            2 => Some(&mut self.register_d),
+            3 => Some(&mut self.register_e),
+            4 => Some(&mut self.register_h),
+            5 => Some(&mut self.register_l),
+            7 => Some(&mut self.register_a),
+            _ => None,
         }
     }
 }
@@ -58,5 +118,74 @@ impl Memory for CPU {
         let high = (data >> 8) as u8;
         self.write(address, low);
         self.write(address + 1, high);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ld_a_b() {
+        let mut cpu = CPU::new();
+        cpu.execute(0b01111000);
+        assert_eq!(cpu.register_a, 0x00);
+    }
+
+    #[test]
+    fn ld_a_d() {
+        let mut cpu = CPU::new();
+        cpu.execute(0b01111010);
+        assert_eq!(cpu.register_a, 0xFF);
+    }
+
+    #[test]
+    fn ld_b_l() {
+        let mut cpu = CPU::new();
+        cpu.execute(0x45);
+        assert_eq!(cpu.register_b, 0x0D);
+    }
+
+    #[test]
+    fn ld_c_a() {
+        let mut cpu = CPU::new();
+        cpu.execute(0x4f);
+        assert_eq!(cpu.register_c, 0x11);
+    }
+
+    #[test]
+    fn ld_d_h() {
+        let mut cpu = CPU::new();
+        cpu.execute(0x54);
+        assert_eq!(cpu.register_d, 0x00);
+    }
+
+    #[test]
+    fn ld_e_c() {
+        let mut cpu = CPU::new();
+        cpu.execute(0x59);
+        assert_eq!(cpu.register_e, 0x00);
+    }
+
+    #[test]
+    fn ld_h_e() {
+        let mut cpu = CPU::new();
+        cpu.execute(0x63);
+        assert_eq!(cpu.register_h, 0x56);
+    }
+
+    #[test]
+    fn ld_l_a() {
+        let mut cpu = CPU::new();
+        cpu.execute(0x6F);
+        assert_eq!(cpu.register_l, 0x11);
+    }
+
+    #[test]
+    fn ld_a_de() {
+        let mut cpu = CPU::new();
+        cpu.execute(0x1A);
+        let val = cpu.read(CPU::combine_bytes(cpu.register_d, cpu.register_e));
+        assert_eq!(cpu.register_a, val);
     }
 }
